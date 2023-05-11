40a8ccef2b2aaae5a8234b2d712e32007689443e
Paulo Costa <pjrc83@gmail.com>
2023-05-11 12:12:52 -0300

    Definido modelo

--- src/main/java/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
+++ src/main/java/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
@@ -15,13 +15,23 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/novaEmpresa")
 public class NovaEmpresaServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		Empresa empresa = new Empresa();
 		System.out.println("Cadastrando nova empresa");
+
 		String nomeEmpresa = request.getParameter("nome");
-		PrintWriter out = response.getWriter();
-		out.println("<html><body>Empresa " +nomeEmpresa + " cadastrada com sucesso!</body></html>");
+	
+
+		empresa.setNome(nomeEmpresa);
 		
+		
+		Banco banco = new Banco();
+		banco.adiciona(empresa);
+		PrintWriter out = response.getWriter();
+		out.println("<html><body>Empresa " + nomeEmpresa + " cadastrada com sucesso!</body></html>");
+
 	}
 
 }
