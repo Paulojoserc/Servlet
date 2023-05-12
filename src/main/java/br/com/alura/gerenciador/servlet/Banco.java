13b8d0cc90296d7d567ad28e900b7b8f98d7b4b3
Paulo Costa <pjrc83@gmail.com>
2023-05-12 14:45:40 -0300

    Listando empresas com servlets
    Nessa aula preparamos o modelo da nossa aplicação. O modelo ou domínio são as classes que representam o mundo real, aquilo que o cliente ou usuário da aplicação define.
    
    Para descobrir quais são as classes e funcionalidades do modelo o analista de requisitos conversa com o cliente e documenta cada funcionalidade.
    
    Nesse curso falaremos menos sobre o modelo e a camada de persistência como JDBC ou JPA. Nós vamos focar nas Servlets, claro!

--- src/main/java/br/com/alura/gerenciador/servlet/Banco.java
+++ src/main/java/br/com/alura/gerenciador/servlet/Banco.java
@@ -6,11 +6,23 @@ import java.util.List;
 public class Banco {
 
 	private static List<Empresa> lista = new ArrayList<>();
-	public void adiciona(Empresa empresa) {
 	
-		lista.add(empresa);
-	}
-	public static List<Empresa> getLista() {
+		static {
+	        Empresa empresa = new Empresa();
+	        empresa.setNome("Alura");
+	        Empresa empresa2 = new Empresa();
+	        empresa2.setNome("Caelum");
+	        lista.add(empresa);
+	        lista.add(empresa2);
+
+
+	    }
+
+	    public void adiciona (Empresa empresa) { 
+	        Banco.lista.add(empresa);
+	    }
+	
+	public static List<Empresa> getEmpresas() {
 		return Banco.lista;
 	}
 
--- src/main/java/br/com/alura/gerenciador/servlet/Empresa.java
+++ src/main/java/br/com/alura/gerenciador/servlet/Empresa.java
@@ -3,26 +3,19 @@ package br.com.alura.gerenciador.servlet;
 public class Empresa {
 
 	private Integer id;
-	private String nome;
+    private String nome;
 
-	public Empresa() {
-		
-	}
-	
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
 }
