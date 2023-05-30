370d6770fba3c628d5ed59cd52b74c4a9e58e756
Paulo Costa <pjrc83@gmail.com>
2023-05-30 16:27:00 -0300

    Melhorando o controlador

--- src/main/java/br/com/alura/gerenciador/servlet/UnicaEntradaServlet.java
+++ src/main/java/br/com/alura/gerenciador/servlet/UnicaEntradaServlet.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import br.com.alura.gerenciador.acao.Acao;
 import br.com.alura.gerenciador.acao.AlteraEmpresa;
 import br.com.alura.gerenciador.acao.ListaEmpresas;
 import br.com.alura.gerenciador.acao.MostraEmpresa;
@@ -24,29 +25,18 @@ public class UnicaEntradaServlet extends HttpServlet {
 			throws ServletException, IOException {
 
 		String paramAcao = request.getParameter("acao");
-		String nome = null;
-		if (paramAcao.equals("ListaEmpresas")) {
-
-			ListaEmpresas acao = new ListaEmpresas();
-			nome = acao.executa(request, response);
-
-		} else if (paramAcao.equals("RemoveEmpresa")) {
-
-			RemoveEmpresa acao = new RemoveEmpresa();
-			nome = acao.executa(request, response);
-		} else if (paramAcao.equals("MostraEmpresa")) {
-			MostraEmpresa acao = new MostraEmpresa();
-			nome = acao.executa(request, response);
-		} else if (paramAcao.equals("AlteraEmpresa")) {
-			AlteraEmpresa acao = new AlteraEmpresa();
-			nome = acao.executa(request, response);
-		} else if (paramAcao.equals("NovaEmpresa")) {
-			NovaEmpresa acao = new NovaEmpresa();
-			nome = acao.executa(request, response);
-		}else if (paramAcao.equals("NovaEmpresaForm")) {
-			NovaEmpresaForm acao = new NovaEmpresaForm();
-			nome = acao.executa(request, response);
+		
+		String nomeDaClasse = "br.com.alura.gerenciador.acao."+ paramAcao;
+		
+		Acao acao;
+		try {
+			Class classe = Class.forName(nomeDaClasse);//carrega a classe com o nome
+			acao = (Acao) classe.newInstance();
+		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
+			throw new ServletException(e);
 		}
+		
+		String nome = acao.executa(request, response);
 		
 		String[] tipoEEndereco = nome.split(":");
 		if (tipoEEndereco[0].equals("forward")) {
@@ -56,6 +46,7 @@ public class UnicaEntradaServlet extends HttpServlet {
 			response.sendRedirect(tipoEEndereco[1]);
 
 		}
+				
 	}
 
 }
