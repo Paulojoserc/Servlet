08963978cad69eec34bffc2b6e2276cd001e6c53
Paulo Costa <pjrc83@gmail.com>
2023-06-05 17:04:21 -0300

    Ordem de execução

--- src/main/java/br/com/alura/gerenciador/servlet/UnicaEntradaServlet.java
+++ src/main/java/br/com/alura/gerenciador/servlet/UnicaEntradaServlet.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpSession;
 import br.com.alura.gerenciador.acao.Acao;
 
 
-@WebServlet(urlPatterns="/entrada")
+//@WebServlet(urlPatterns="/entrada")
 public class UnicaEntradaServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
@@ -22,15 +22,15 @@ public class UnicaEntradaServlet extends HttpServlet {
 
 		  String paramAcao = request.getParameter("acao");
 			
-			HttpSession sessao = request.getSession();
-			boolean usuarioNaoEstaLogado = (sessao.getAttribute("usuarioLogado") == null);
-			boolean ehUmaAcaoProtegida = !(paramAcao.equals("Login") || paramAcao.equals("LoginForm"));
-			
-			if(ehUmaAcaoProtegida && usuarioNaoEstaLogado) {
-				response.sendRedirect("entrada?acao=LoginForm");
-				return;
-			
-	        }
+//			HttpSession sessao = request.getSession();
+//			boolean usuarioNaoEstaLogado = (sessao.getAttribute("usuarioLogado") == null);
+//			boolean ehUmaAcaoProtegida = !(paramAcao.equals("Login") || paramAcao.equals("LoginForm"));
+//			
+//			if(ehUmaAcaoProtegida && usuarioNaoEstaLogado) {
+//				response.sendRedirect("entrada?acao=LoginForm");
+//				return;
+//			
+//	        }
 
 	        String nomeDaClasse = "br.com.alura.gerenciador.acao." + paramAcao;
 
